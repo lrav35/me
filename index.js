6e42ed445e9ec515eaef2bb7cabb72c74610b7f6
Luc Ravenelle <lrav35@gmail.com>
2024-08-06 16:40:36 -0400

    adding list capability and another button for demo

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const app = new Elysia()
 	.listen(3000);
 
 // serve content via str response
-app.get('/dynamic-content', () => '<p>This content was loaded dynamic with hypermedia via htmx!!!</p>');
+app.get('/dynamic-content', () => '<p>This content was loaded dynamically with hypermedia via htmx!!!</p>');
+
+app.get('/pika', () => Bun.file('views/image.html'))
 
 // serve content via file
-app.post('/dynamic-post', handleDynamicPost);
+app.post('/dynamic-post', (body) => handleDynamicPost(body));
 
 console.log(`Elysia is running on port ${app.server.port}`);
 
--- middlewares/dynamicPost.js
+++ middlewares/dynamicPost.js
@@ -1,18 +1,15 @@
-export async function handleDynamicPost({ ctx }) {
+export async function handleDynamicPost({ body }) {
 
-	const body = await ctx.json();
-	// Extract the message from the request body
+
 	const message = body.message || 'default message';
 
-	console.log(body);
-
-	// Generate dynamic HTML with the modified message
 	const htmlContent = `
-        <div class="image-container">
-            <img src="img/pika.png" alt="pikachu" style="width: 150px; margin-top: 10px;">
-            <div class="caption">${message}</div>
-        </div>
-    `;
+	<tr>
+		<td>
+			${message}
+		</td>
+	</tr>
+	`;
 
 	// Return the modified HTML content
 	return new Response(htmlContent, {
